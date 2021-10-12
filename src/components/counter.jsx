fcef3f0fb33dfeaa8e14ff1851ce3f24f26fbada
unknown <you@example.com>
2021-10-12 18:53:56 +0600

    task 3.1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,34 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  const formCount = () => {
-    return count === 0 ? "Ноль" : count;
+const Counter = (props) => {
+  const formValue = () => {
+    return props.value === 0 ? "Ноль" : props.value;
   };
-  const getBagecClasses = () => {
+  const getBadgeClasses = () => {
     let classes = "badge m-2 bg-";
-    classes += count === 0 ? "danger" : "primary";
+    classes += props.value === 0 ? "danger" : "primary";
     return classes;
   };
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-  const handleDecrement = () => {
-    count > 0 ? setCount(count - 1) : formCount();
-  };
+
   return (
     <div>
+      <h4> {props.name}</h4>
       <span
         style={{ fontSize: "20px", fontWeight: "bold" }}
-        className={getBagecClasses()}
+        className={getBadgeClasses()}
       >
-        {formCount()}
+        {formValue()}
       </span>
-      <button onClick={handleIncrement} className="btn btn-secondary btn-sm">
+      <button
+        onClick={() => props.onIncrement(props.id)}
+        className="btn btn-secondary btn-sm"
+      >
         Increment
       </button>
-      <button onClick={handleDecrement} className="btn btn-secondary btn-sm">
+      <button
+        onClick={() => props.onDecrement(props.id)}
+        className="btn btn-secondary btn-sm"
+      >
         Decrement
+      </button>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        Delete
       </button>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Counter from "./components/counter"
-import "bootstrap/dist/css/bootstrap.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Counters from "./components/counters";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
